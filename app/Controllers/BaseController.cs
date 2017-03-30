c4cfda89c84b7c3cbf287bd75dca07e8b0f73b2b
yakimenko <ayakimenko@analit.net>
2017-03-30 13:34:55 +0300

    Добавил DevExpress и шаблон (отчет)

---
+++ app/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+
+namespace Marketing.Controllers
+{
+	public class BaseController : Controller
+	{
+		protected ISession DbSession => HttpContext.Items[typeof(ISession)] as ISession;
+	}
+}
---
+++ app/ViewModels/DateRangeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Marketing.ViewModels
+{
+	public class DateRangeViewModel
+	{
+		public DateTime DateBegin { get; set; }
+		public DateTime DateEnd { get; set; }
+		public List<DateTime> DateBeginDisabledDates { get; set; }
+		public DateTime? DateBeginMax { get; set; }
+		public DateTime? DateBeginMin { get; set; }
+		public List<DateTime> DateEndDisabledDates { get; set; }
+		public DateTime? DateEndMax { get; set; }
+		public DateTime? DateEndMin { get; set; }
+	}
+}
