67f32aab3d839ee093d62ee0b5594e9569596c41
Ивар Лекса <i.lexa@analit.net>
2017-04-03 00:17:02 +0300

    Новшество #67765 Акции производителей частичная реализация списка участников

---
+++ app/ViewModels/MembersGridViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Marketing.ViewModels
+{
+	public class MembersGridViewModel
+	{
+		[Required]
+		public uint MemberId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public int AddressCount { get; set; }
+		public string Subscribes { get; set; }
+	}
+}
