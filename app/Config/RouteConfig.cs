2ddc5966bdade8125f83d53c5e16796d14a467bf
Ивар Лекса <i.lexa@analit.net>
2017-04-04 20:40:49 +0300

    Новшество #67765 Акции производителей условия акции

--- app/Config/RouteConfig.cs
+++ app/Config/RouteConfig.cs
@@ -9,6 +9,7 @@ namespace Marketing
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.IgnoreRoute("{resource}.ashx/{*pathInfo}");
 
 			routes.MapRoute(
 					name: "Default",
